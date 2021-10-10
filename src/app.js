ba6bf65f44654e1dfff176ea0e1c856f02a6349e
fatsalad <olivias669@gmail.com>
2021-10-09 17:17:10 -0700

    added function to format time using timestamp for sunrise sunset

--- src/app.js
+++ src/app.js
@@ -22,6 +22,19 @@ function formatDate() {
   return `${day} ${hours}:${minutes}`;
 }
 
+// Format time for sunrise and sunset using timestamp
+function formatSunriseSunsetTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 // Find and Display Weather, Day, Time by City
 
 function searchCity(city) {
